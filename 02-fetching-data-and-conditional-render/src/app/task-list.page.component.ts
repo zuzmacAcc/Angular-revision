fb439554fcbd4ca51ff7e24b310bca7708e93293
zuzanna.macinska <zuzanna.macinska@accenture.com>
2024-07-18 11:18:41 +0200

    fetch data from fake backend, ng if - else (for loading)

---
+++ 02-fetching-data-and-conditional-render/src/app/task-list.page.component.ts
@@ -0,0 +1,47 @@
+import { Component } from "@angular/core";
+import { TasksListComponent } from "./tasks-list.component";
+import { SubmitTextComponent } from "./submit-text.component";
+import { Task } from "./Task";
+import { NgIf } from "@angular/common";
+
+@Component({
+  selector: "app-task-list-page",
+  standalone: true,
+  imports: [TasksListComponent, SubmitTextComponent, NgIf],
+  template: `
+    <app-submit-text (submitText)="addTask($event)" />
+    <app-tasks-list *ngIf="!loading"  class="block mt-4" [tasks]="tasks" />
+    <p *ngIf="loading; else loadingTemplate" >Loading...</p>
+
+    <ng-template #loadingTemplate>
+      <p>hej</p>
+    </ng-template>
+    
+  `,
+})
+export class TaskListPageComponent {
+  tasks: Task[] = [];
+
+  loading = false;
+
+  private readonly URL = "http://localhost:3000";
+
+  constructor() {
+    this.loading = true;
+    fetch(`${this.URL}/tasks`).then(response => {
+      return response.json();
+    }).then(response => {
+      setTimeout(() => {
+        this.tasks = response;
+        this.loading = false;  
+      }, 1200);
+    });
+  }
+
+  addTask(name: string) {
+  //   this.tasks.push({
+  //     name,
+  //     done: false,
+  //   });
+   }
+}
